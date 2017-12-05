ab67561e79b902f616b90fda06a9ec8ae656b6fe
ThomasDenis <thomas2.denis@gmail.com>
2017-12-05 18:29:01 +0100

    Ajout du formulaire d'ajout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import {OptionComponent} from "./option/option.component";
 import {OptionsComponent} from "./options/options.component";
+import {FormComponent} from "./shared/form/form.component";
 
 const ROUTES: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'option/:id', component: OptionComponent },
-    { path: 'options', component: OptionsComponent }
+    { path: 'options', component: OptionsComponent },
+    { path: 'ajout', component: FormComponent }
 ];
 
 export const APP_ROUTES = RouterModule.forRoot(ROUTES, { useHash: false });
