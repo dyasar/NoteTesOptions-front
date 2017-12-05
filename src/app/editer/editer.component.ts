597c3237ad2cba552b1166eae146163666fb9347
ThomasDenis <thomas2.denis@gmail.com>
2017-12-05 22:46:06 +0100

    correction

--- src/app/editer/editer.component.ts
+++ src/app/editer/editer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editer.component.css']
 })
 export class EditerComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
